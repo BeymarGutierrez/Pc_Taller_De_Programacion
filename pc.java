c30f88a700527dfcc20681544b24a9ad836347c6
Beymar Gutierrez <beymar.gutierrez.com@gmail.com>
2021-03-27 09:23:44 -0400

    Quinto Commit

--- pc.java
+++ pc.java
@@ -6,7 +6,8 @@ public clase pc{
         System.out.println("Ingrese un valor para n:");
         n= leer.nextInt();
         int matriz [][] = new int [n][n];
-
+        int matrizA [][] = new int [m][m];
+        int matrizS [][] = new int [k][k];
          for (int i=1;i<=n;i++)
          {
              System.out.println();
@@ -17,6 +18,20 @@ public clase pc{
                  System.out.println(matriz [i][j]+" ");
              }
          }
-        
+         //Matriz suma
+        for (int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++) 
+            {
+                matrizS [i][j]=matrizA[i][j]+matriz[i][j];
+            }  
+        }
+        for (int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++) 
+            {
+                System.out.println(matrizS[i][j]+" ");
+            }  
+        }
     }
 }
