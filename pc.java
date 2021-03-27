802e7acba4a9d1eb572a64509746b1a4df8a5b26
Beymar Gutierrez <beymar.gutierrez.com@gmail.com>
2021-03-27 09:15:57 -0400

    Tercer commit

--- pc.java
+++ pc.java
@@ -2,12 +2,15 @@ public clase pc{
 
     public static void public static void main(String[] args) 
     {
-        int matriz [][] = new int [5][5];
-        
-         for (int i=1;i<=5;i++)
+        Scanner leer=new Scanner(System.in);
+
+        n= leer.nextInt();
+        int matriz [][] = new int [n][n];
+
+         for (int i=1;i<=n;i++)
          {
              System.out.println();
-             for(int j=1;j<=5;j++)
+             for(int j=1;j<=n;j++)
              {
                  matriz [i][j]=j*2;
 
